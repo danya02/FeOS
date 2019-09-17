aa8908267dad59afc76c8431181c4621f1470834
Danya Generalov <danya@danya02.ru>
2019-09-17 13:17:25 +0300

    Adding interrupts (almost identical to blog).

--- fe_os/src/main.rs
+++ fe_os/src/main.rs
@@ -8,6 +8,8 @@ use lazy_static::lazy_static;
 
 lazy_static! {
 
+    static ref WHITE_ON_BLACK:ColorCode = ColorCode::new(Color::White, Color::Black);
+    static ref BLACK_ON_WHITE:ColorCode = ColorCode::new(Color::Black, Color::White);
     static ref RED_ON_BLACK:ColorCode = ColorCode::new(Color::LightRed, Color::Black);
     static ref BLACK_ON_RED:ColorCode = ColorCode::new(Color::Black, Color::LightRed);
 }
@@ -25,7 +27,12 @@ static HELLO : &str = "This is a test string, Hello x86_64!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::WRITER.lock().write_string(HELLO);
+    fe_os::init();
+    for i in HELLO.bytes() {
+        if vga_buffer::WRITER.lock().color_code == *BLACK_ON_WHITE { vga_buffer::WRITER.lock().color_code = *WHITE_ON_BLACK; } else {vga_buffer::WRITER.lock().color_code = *BLACK_ON_WHITE; }
+        vga_buffer::WRITER.lock().write_byte(i);
+    }
+    x86_64::instructions::interrupts::int3();
     assert_eq!(0,1);
     loop {}
 }
