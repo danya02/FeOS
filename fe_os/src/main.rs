15d73108d3d6bdfd312e45124ccc347ed80a9aa3
Danya Generalov <danya@danya02.ru>
2019-09-17 15:26:58 +0300

    Adding protection from double faults at stack overflow.

--- fe_os/src/main.rs
+++ fe_os/src/main.rs
@@ -32,7 +32,11 @@ pub extern "C" fn _start() -> ! {
         if vga_buffer::WRITER.lock().color_code == *BLACK_ON_WHITE { vga_buffer::WRITER.lock().color_code = *WHITE_ON_BLACK; } else {vga_buffer::WRITER.lock().color_code = *BLACK_ON_WHITE; }
         vga_buffer::WRITER.lock().write_byte(i);
     }
-    x86_64::instructions::interrupts::int3();
+    
+    fn stack_overflow() { stack_overflow(); }
+
+    stack_overflow();
+
     assert_eq!(0,1);
     loop {}
 }
